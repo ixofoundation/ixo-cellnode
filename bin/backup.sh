69ca1a64d9cdbffb19a9784e21e350f97d3cb096
Brennon Hampton <brennon.hampton@trustlab.tech>
2018-07-19 17:25:38 +0200

    clean up

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,1 +1,13 @@
+echo "***********************************"
+echo "* ELYSIAN BACKUP                  *"
+echo "***********************************"
+echo ""
+echo "Backup database to $HOME"
+echo ""
+
 docker run --rm --net ixopds_default --link db:db -v /$HOME/backup:/backup mongo bash -c 'mongodump --out /backup --host db:27017'
+
+echo ""
+echo "***********************************"
+echo "* ELYSIAN BACKUP COMPLETE         *"
+echo "***********************************"
--- bin/restore.sh
+++ bin/restore.sh
@@ -1,1 +1,12 @@
+echo "***********************************"
+echo "* ELYSIAN RESTORE                 *"
+echo "***********************************"
+echo ""
+echo "Restore database from $HOME"
+echo ""
 docker run --rm --net ixopds_default --link db:db -v /$HOME/backup:/backup mongo bash -c 'mongorestore /backup --host db:27017'
+
+echo ""
+echo "***********************************"
+echo "* ELYSIAN RESTORE COMPLETE        *"
+echo "***********************************"
