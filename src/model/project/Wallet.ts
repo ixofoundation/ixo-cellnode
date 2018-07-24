d1e934b4bcdfdb0696b684f22a1e31b207fe7ac8
Brennon Hampton <brennon.hampton@trustlab.tech>
2018-07-24 14:44:42 +0200

    encrypt wallet private key

--- src/model/project/Wallet.ts
+++ src/model/project/Wallet.ts
@@ -3,16 +3,37 @@ import { IWallet } from "./IWallet";
 
 export interface IWalletModel extends IWallet, Document { }
 
+var crypto = require('crypto');
+
+function encrypt(text: string) {
+    var cipher = crypto.createCipher('aes-256-cbc', "trustlab.tech");
+    var crypted = cipher.update(text, 'utf8', 'hex');
+    crypted += cipher.final('hex');
+    return crypted;
+}
+
+function decrypt(text: string) {
+    if (text === null || typeof text === 'undefined') { return text; };
+    var decipher = crypto.createDecipher('aes-256-cbc', "trustlab.tech");
+    var dec = decipher.update(text, 'hex', 'utf8');
+    dec += decipher.final('utf8');
+    return dec;
+}
+
 export var WalletSchema: Schema = new Schema({
 
     did: String,
-    signKey: String,
+    signKey: {
+        type: String,
+        get: decrypt, 
+        set: encrypt
+    },
     verifyKey: String
 
 }, { strict: false });
 
+export const Wallet: Model<IWalletModel> = model<IWalletModel>("Wallet", WalletSchema);
+
 WalletSchema.pre("save", function (next) {
     next();
 });
-
-export const Wallet: Model<IWalletModel> = model<IWalletModel>("Wallet", WalletSchema);
