faebe5aeff8edd4fa6ed411bcd91400227e1720e
Brennon Hampton <brennon.hampton@trustlab.tech>
2018-08-01 16:30:56 +0200

    send volume information for backup and restore

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
+DIR=$1
+if [ -z "$1" ]
+  then
+    echo "please supply directory where backup needs to be saved"
+    exit 1
+fi
 echo "***********************************"
 echo "* ELYSIAN BACKUP                  *"
 echo "***********************************"
 echo ""
-echo "Backup database to $HOME"
+echo "Backup database to $DIR/backup"
 echo ""
-
-docker run --rm --net ixopds_default --link db:db -v /$HOME/backup:/backup mongo bash -c 'mongodump --out /backup --host db:27017'
+docker run --rm --net ixopds_default --link db:db -v $DIR/backup:/backup mongo bash -c 'mongodump --out /backup --host db:27017'
 
 echo ""
 echo "***********************************"
--- bin/restore.sh
+++ bin/restore.sh
@@ -1,10 +1,17 @@
+#!/bin/bash
+DIR=$1
+if [ -z "$1" ]
+  then
+    echo "please supply directory where backup resides"
+    exit 1
+fi
 echo "***********************************"
 echo "* ELYSIAN RESTORE                 *"
 echo "***********************************"
 echo ""
-echo "Restore database from $HOME"
+echo "Restore database from $DIR/backup"
 echo ""
-docker run --rm --net ixopds_default --link db:db -v /$HOME/backup:/backup mongo bash -c 'mongorestore /backup --host db:27017'
+docker run --rm --net ixopds_default --link db:db -v $DIR/backup:/backup mongo bash -c 'mongorestore /backup --host db:27017'
 
 echo ""
 echo "***********************************"
