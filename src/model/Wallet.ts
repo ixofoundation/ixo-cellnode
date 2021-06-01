6a1fcc4d7a1c05b0596331f2ecaf9922fb8ca51d
migueldingli1997 <migueldingli97@gmail.com>
2021-06-01 16:07:47 +0200

    Update encrypt/decrypt functions due to deprecated crypto function

--- src/model/Wallet.ts
+++ src/model/Wallet.ts
@@ -1,6 +1,8 @@
 import {Document, Model, model, Schema} from "mongoose";
 import {IWallet} from "./IWallet";
-import {createCipher, createDecipher} from 'crypto';
+import {createCipheriv, createDecipheriv, randomBytes} from 'crypto';
+
+const IV_LENGTH = 16;
 
 export interface IWalletModel extends IWallet, Document {
 }
@@ -9,17 +11,23 @@ const ASYM_CYPHER = (process.env.ASYM_CYPHER || 'aes-256-cbc');
 const ASYM_KEY = (process.env.ASYM_KEY || 'trustlab.tech');
 
 function encrypt(text: string) {
-  const cipher = createCipher(ASYM_CYPHER, ASYM_KEY);
-  return cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
+  // Source: https://www.debugcn.com/en/article/38676742.html
+  let iv = randomBytes(IV_LENGTH);
+  let cipher = createCipheriv(ASYM_CYPHER, Buffer.from(ASYM_KEY, 'hex'), iv);
+  let encrypted = cipher.update(text);
+  encrypted = Buffer.concat([encrypted, cipher.final()]);
+  return iv.toString('hex') + ':' + encrypted.toString('hex');
 }
 
 function decrypt(text: string) {
-  if (text === null || typeof text === 'undefined') {
-    return text;
-  }
-
-  const decipher = createDecipher(ASYM_CYPHER, ASYM_KEY);
-  return decipher.update(text, 'hex', 'utf8') + decipher.final('utf8');
+  // Source: https://www.debugcn.com/en/article/38676742.html
+  let textParts = text.split(':');
+  let iv = Buffer.from(textParts[0], 'hex');
+  let encryptedText = Buffer.from(textParts.slice(1).join(':'), 'hex');
+  let decipher = createDecipheriv(ASYM_CYPHER, Buffer.from(ASYM_KEY, 'hex'), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
 }
 
 export var WalletSchema: Schema = new Schema({
