71964055551472dda9834172a49d773bbd777788
Lohan Stadler <lohanjs@outlook.com>
2023-02-15 13:39:16 +0200

    fix: allow cors in helmet

--- src/App.ts
+++ src/App.ts
@@ -68,7 +68,11 @@ class App {
                 context: { prisma },
             }),
         );
-        this.express.use(helmet());
+        this.express.use(
+            helmet({
+                crossOriginResourcePolicy: false,
+            }),
+        );
         const limiter = rateLimit({
             windowMs: 15 * 60 * 1000, // 15 minutes
             max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
