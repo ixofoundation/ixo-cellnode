fee9b805c6a197455b1e8202eaa2b99385f06c46
Lohan Stadler <lohanjs@outlook.com>
2023-02-17 08:45:29 +0200

    fix: increase rate limit

--- src/App.ts
+++ src/App.ts
@@ -75,7 +75,7 @@ class App {
         );
         const limiter = rateLimit({
             windowMs: 15 * 60 * 1000, // 15 minutes
-            max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+            max: 100000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
             standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
             legacyHeaders: false, // Disable the `X-RateLimit-*` headers
         });
