3bdb375ff1cb562e887f7eb8e7b15d262e74bf6d
Michael Pretorius <michael@ixo.world>
2023-06-05 09:03:12 +0200

    chore: update rate limit

--- src/App.ts
+++ src/App.ts
@@ -65,8 +65,8 @@ class App {
 			}),
 		);
 		const limiter = rateLimit({
-			windowMs: 15 * 60 * 1000, // 15 minutes
-			max: 100000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+			windowMs: 1 * 60 * 1000, // 1 minutes
+			max: 10000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 			standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 			legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 		});
